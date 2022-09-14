0d547e518ec2812dd7d2cc1e6f5e6f342bf45810
Andrea Lin <andre93.lin@gmail.com>
2022-09-14 11:33:24 -0300

    add mui dark theme with primary and secondary colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import Home from './screens/Home';
 import Info from './screens/Info';
@@ -6,12 +8,28 @@ import Login from './screens/Login';
 import Repositories from './screens/Repositories';
 import Repository from './screens/Repository';
 
-import Button from '@mui/material/Button';
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#40c4ff',
+      light: '#66cfff',
+      dark: '#2c89b2',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#ec407a',
+      light: '#ef6694',
+      dark: '#a52c55',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App() {
   return (
-    <>
-      <Button variant="outlined">Test MUI button</Button>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="/" element={<Home />}>
@@ -21,7 +39,7 @@ function App() {
           <Route path=":repositoryId/details" element={<h1>Details</h1>} />
         </Route>
       </Routes>
-    </>
+    </ThemeProvider>
   );
 }
 
